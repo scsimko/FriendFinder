36cef8a838789a804730a1b71e93c1263c098a58
scsimko <stephensimko.bootcamp@gmail.com>
2018-10-29 21:34:47 -0400

    Updates

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//Store data
+const friendArray = [
+    {
+      name: "Action",
+      photo: "http://www2.pictures.zimbio.com/gi/Action+Bronson+Olmeca+Altos+J+Hernandez+Sons+tytEihA5aYKl.jpg",
+      scores: ["5", "1", "5", "1", "5", "1", "5", "2", "5", "1"]
+    },
+    {
+      name: "Charles",
+      photo: "http://www3.pictures.zimbio.com/bg/7th+Annual+Race+to+Erase+MS+025t1NHCgx_m.jpg",
+      scores: ["3", "3", "3", "3", "5", "5", "3", "5", "5", "5"]
+    },
+    {
+      name: "TheReportOfTheWeek",
+      photo: "https://i.kym-cdn.com/entries/icons/original/000/023/054/reviewbrahhlol.JPG",
+      scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    }
+  ];
+  
+  module.exports = friendArray
+  
