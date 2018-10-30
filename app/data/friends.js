47a29ea3ceea1436048b6e06edb09b7255fcf76b
scsimko <stephensimko.bootcamp@gmail.com>
2018-10-29 22:07:15 -0400

    Updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,34 @@
 //Store data
-const friendArray = [
-    {
-      name: "Action",
-      photo: "http://www2.pictures.zimbio.com/gi/Action+Bronson+Olmeca+Altos+J+Hernandez+Sons+tytEihA5aYKl.jpg",
-      scores: ["5", "1", "5", "1", "5", "1", "5", "2", "5", "1"]
-    },
-    {
-      name: "Charles",
-      photo: "http://www3.pictures.zimbio.com/bg/7th+Annual+Race+to+Erase+MS+025t1NHCgx_m.jpg",
-      scores: ["3", "3", "3", "3", "5", "5", "3", "5", "5", "5"]
-    },
-    {
-      name: "TheReportOfTheWeek",
-      photo: "https://i.kym-cdn.com/entries/icons/original/000/023/054/reviewbrahhlol.JPG",
-      scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-    }
-  ];
-  
-  module.exports = friendArray
-  
+const friendArray = [{
+    name: "Action",
+    photo: "http://www2.pictures.zimbio.com/gi/Action+Bronson+Olmeca+Altos+J+Hernandez+Sons+tytEihA5aYKl.jpg",
+    scores: ["5", "1", "5", "1", "5", "1", "5", "2", "5", "1"]
+  },
+  {
+    name: "Charles",
+    photo: "http://www3.pictures.zimbio.com/bg/7th+Annual+Race+to+Erase+MS+025t1NHCgx_m.jpg",
+    scores: ["3", "3", "3", "3", "5", "5", "3", "5", "5", "5"]
+  },
+  {
+    name: "TheReportOfTheWeek",
+    photo: "https://i.kym-cdn.com/entries/icons/original/000/023/054/reviewbrahhlol.JPG",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "Lebron",
+    photo: "http://www2.pictures.zimbio.com/gi/LeBron+James+Golden+State+Warriors+vs+Los+qpPoVe6PVj1l.jpg",
+    scores: ["2", "1", "3", "1", "5", "2", "1", "2", "3", "1"]
+  },
+  {
+    name: "Donald",
+    photo: "http://www2.pictures.zimbio.com/gi/European+Best+Pictures+Day+October+27+2018+MRr0Kvwd42_l.jpg",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "Daniel",
+    photo: "http://www4.pictures.zimbio.com/gi/Daniel+Bryan+WWE+SuperStars+Kids+Gy0I2-s4WTZl.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  }
+];
+
+module.exports = friendArray
